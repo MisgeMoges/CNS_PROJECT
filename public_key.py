7c926ace3c1e49811950b0bfd9b4bd631ca2a36e
Misgan Moges <misganmoges03@gmail.com>
2023-04-11 15:48:03 +0300

    second message

---
+++ public_key.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives import serialization, hashes
+
+# Generate a public-private key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+public_key = private_key.public_key()
+
+# Serialize the public key to PEM format
+public_key_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Encrypt a message using the public key
+message = b'This is a secret message.'
+encrypted_message = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Decrypt the message using the private key
+decrypted_message = private_key.decrypt(
+    encrypted_message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print('Original message:', message)
+print('Encrypted message:', encrypted_message)
+print('Decrypted message:', decrypted_message)
